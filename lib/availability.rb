f9abe769da77805549d60c44155ad57df4d7b478
matt vickers <matt.vickers@sky.com>
2016-11-18 17:56:45 +0000

    parses JSON and validates time

--- lib/availability.rb
+++ lib/availability.rb
@@ -1,1 +1,18 @@
+#!/usr/bin/env ruby
+
 require 'json'
+
+time = ARGV[0].to_i
+
+def load_hash
+  file = File.read('./availability_slots.json')
+  slots_hash = JSON.parse(file)
+end
+
+def check_valid_time(time)
+  if time < 8 || time > 15
+    fail "Appointments not available before 8am or after 3pm"
+  end
+end
+
+load_hash
--- spec/availability_spec.rb
+++ spec/availability_spec.rb
@@ -1,1 +1,15 @@
 require 'availability'
+
+describe '#load_hash' do
+  it 'parses JSON file into Ruby hash' do
+    expect(load_hash).to have_key('availability_slots')
+  end
+end
+
+describe '#check_valid_time' do
+  msg = 'Appointments not available before 8am or after 3pm'
+  it 'raises an error if time is outside available hours' do
+    expect { check_valid_time(7) }.to raise_error msg
+    expect { check_valid_time(16) }.to raise_error msg
+  end
+end

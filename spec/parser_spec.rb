bd2a04ec706470194d6ee708d7984d2621f3591d
matt vickers <matt.vickers@sky.com>
2016-11-19 14:01:49 +0000

    tests fixed

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -10,7 +10,7 @@ describe Parser do
       expect(parser.instance_variable_get(:@available_slots)).to eq []
     end
 
-    it 'parses JSON times into Ruby hash' do
+    it 'parses and stores JSON times into array' do
       parser.load_hash
       expect(parser.instance_variable_get(:@available_slots)).to include("08:00:00")
     end
